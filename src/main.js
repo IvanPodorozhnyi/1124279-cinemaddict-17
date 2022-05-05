e490454b38c8fcf58dbbf97960f7ff8e7cee9044
Ivan Podorozhnyi <ivanpodorozhnj@gmail.com>
2022-05-05 20:09:38 +0300

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import {render} from './render.js';
 import ProfileRatingView from './view/profile-rating-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
-
 const boardPresenter = new BoardPresenter();
 const siteMainElement = document.querySelector('.main');
 const siteHeader = document.querySelector('.header');
+import FilmModel from './model/film-model.js';
+
+const filmModel = new FilmModel();
+
+// console.log(new FilmModel().getFilms());
 
 render(new ProfileRatingView, siteHeader);
 
-boardPresenter.init(siteMainElement);
+boardPresenter.init(siteMainElement, filmModel);
 
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -7,30 +7,49 @@ import FilmView from '../view/film-view.js';
 import MainNavigationView from '../view/main-navigation-view.js';
 import PopupView from '../view/popup-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
-
-const CARD_QUANTITY = 5;
+import FilmListView from '../view/film-list-view.js';
+import FilmContainerView from '../view/film-list-container-view.js';
 
 
 export default class BoardPresenter {
+  filmContainer = new FilmContainerView();
+  filmList = new FilmListView();
+  filmWrapper = new FilmView();
 
-  init = (boardContainer) => {
 
+  init = (boardContainer, filmModel) => {
     this.boardContainer = boardContainer;
+    this.filmModel = filmModel;
+    this.boardFilms = [...this.filmModel.getFilms()];
+    this.filmComments = [...this.filmModel.getComments()];
 
     render(new MainNavigationView(), this.boardContainer);
-
     render(new FilterView(), this.boardContainer);
+    render(this.filmWrapper, this.boardContainer);
+    render(this.filmList, this.filmWrapper.getElement());
+    render(this.filmContainer, this.filmList.getElement());
 
-    render(new FilmView(), this.boardContainer);
+    for (let i = 0; i < this.boardFilms.length; i++) {
+      render(new FilmCardView(this.boardFilms[i]), this.filmContainer.getElement());
 
-    const filmList = document.querySelector('.films-list__container'); // Не нравится мне это, но пока не дошло как можно сделать иначе.
-
-    for (let i = 0; i < CARD_QUANTITY; i++) {
-      render(new FilmCardView(), filmList);
     }
-    render(new ShowMoreButtonView, filmList);
+    render(new ShowMoreButtonView, this.filmContainer.getElement());
 
-    render(new PopupView, this.boardContainer);
+    const cardsFilms = document.querySelectorAll('.film-card');
+
+    cardsFilms.forEach((element) => {
+      element.addEventListener('click', (evt) => {
+        const cardId = evt.target.getAttribute('data-id');
+        this.boardFilms.forEach((film) => {
+          if (Number(film.id) === Number(cardId)) {
+            render(new PopupView(film, this.filmComments), this.boardContainer);
+            document.querySelector('.film-details__close-btn').addEventListener('click', () => {
+              document.querySelector('.film-details').remove();
+            });
+          }
+        });
+      });
+    });
   };
 }
 
